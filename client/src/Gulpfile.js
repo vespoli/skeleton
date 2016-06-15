15366738ae77871f0ba68e7e13c3fdbfb2e2d448
David Vespoli <david.vespoli@gmail.com>
2016-06-14 20:31:18 -0700

    tweaks to gulpfile. added minify manual task

--- client/src/Gulpfile.js
+++ client/src/Gulpfile.js
@@ -8,11 +8,12 @@ var gulp    = require('gulp'),
     notify  = require('gulp-notify'),
     autoprefixer = require('gulp-autoprefixer'),
     include = require('gulp-include'),
-    connect = require('gulp-connect'),
     concat = require('gulp-concat'),
     changed = require('gulp-changed'),
     plumber = require('gulp-plumber'),
-    rename = require("gulp-rename");
+    rename = require("gulp-rename"),
+    minify = require('gulp-minifier'),
+    buildDirectory = '../build';
 
 
 gulp.task('lint', function() {
@@ -26,7 +27,7 @@ gulp.task('scripts', function() {
     .pipe(plumber())
     .pipe(include())
     .pipe(concat('main.js'))
-    .pipe(gulp.dest('../build/js'));
+    .pipe(gulp.dest(buildDirectory + '/js'));
 });
 
 gulp.task('styles', function() {
@@ -36,10 +37,9 @@ gulp.task('styles', function() {
     .pipe(sass({onError: function(e) { console.log(e); } }))
     .pipe(autoprefixer('last 2 versions', '> 1%', 'ie 8'))
     .pipe(rename('main.css'))
-    .pipe(gulp.dest('../build/styles'));
+    .pipe(gulp.dest(buildDirectory + '/styles'));
     
 });
-
 
 gulp.task('markup', function() {
   gulp.src(['./templates/pages/**/*.jade','./templates/*.jade'])
@@ -47,34 +47,38 @@ gulp.task('markup', function() {
     .pipe(jade({
       pretty: true
     }))
-    .pipe(gulp.dest('../build/html'));
-});
-
-
-gulp.task('server', function() {
-  connect.server({
-    livereload: true,
-    root: '../build'
-  });
+    .pipe(gulp.dest(buildDirectory + '/html'));
 });
 
 gulp.task('copy-data', function() {
   gulp.src('./data/*.json')
     .pipe(changed('./data/*.json'))
-    .pipe(gulp.dest('../build/data'));
+    .pipe(gulp.dest(buildDirectory + '/data'));
 });
 
 gulp.task('copy-lib', function() {
   gulp.src('./js/lib/*')
     .pipe(changed('./js/lib/*'))
-    .pipe(gulp.dest('../build/js/lib'));
+    .pipe(gulp.dest(buildDirectory + '/js/lib'));
 });
 
 gulp.task('copy-media', function() {
   gulp.src('./media/**/*')
     .pipe(changed('./media/**/*'))
-    .pipe(gulp.dest('../build/media'));
+    .pipe(gulp.dest(buildDirectory + '/media'));
 });
+
+//this needs to be run manually
+gulp.task('minify', function() {
+  gulp.src(buildDirectory + '/**/*').pipe(minify({
+    minify: true,
+    collapseWhitespace: true,
+    conservativeCollapse: true,
+    minifyJS: true,
+    minifyCSS: true
+  })).pipe(gulp.dest(buildDirectory + '/'));
+});
+
 
 gulp.task('watch', function() {
 
@@ -103,6 +107,6 @@ gulp.task('watch', function() {
 
 });
 
-gulp.task('default', ['scripts', 'styles', 'markup', 'server', 'copy-data', 'copy-lib', 'copy-media', 'watch' ]);
+gulp.task('default', ['scripts', 'styles', 'markup', 'copy-data', 'copy-lib', 'copy-media', 'watch' ]);
 
 
