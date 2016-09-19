d0919814850e0a8263fcbd1136bf1512e2d51b3a
David Vespoli <david.vespoli@gmail.com>
2016-09-19 09:08:45 -0700

    more forms work

--- client/src/templates/pages/form-elements/controller.js
+++ client/src/templates/pages/form-elements/controller.js
@@ -1,10 +1,22 @@
 angular
   .module('skellieApp')
   .controller('FormElementsCtrl', function($scope) {
+    $scope.aliens = [
+      'ET',
+      'Alf',
+      'Predator',
+      'Uncle Martin',
+      'Paul',
+      'Gredo',
+      'Yoda',
+      'Chewbacca',
+      'Mr. Spock'
+    ];
     $scope.user = {
       phone: '',
       email: '',
       firstName: '',
       lastName: '' ,
+      alien: ''
     };
   });
